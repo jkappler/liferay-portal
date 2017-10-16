820cd952488a3e9e41cad57d2d39de7f22db777c
arthurchan35 <arthurchan35@gmail.com>
2017-10-16 10:44:50 +0800

    LPS-28940 Add a simple factory to handle runtime object instatiation. It maybe better upgrading to an abstract factory in the future, when they are more different sub-displaycontext

---
+++ modules/apps/youtube/youtube-web/src/main/java/com/liferay/youtube/web/internal/display/context/VideoEmbedderDisplayContextSimpleFactory.java
@@ -0,0 +1,22 @@
+package com.liferay.youtube.web.internal.display.context;
+
+import com.liferay.youtube.web.configuration.VideoEmbedderConfiguration;
+import javax.portlet.PortletPreferences;
+import javax.servlet.http.HttpServletRequest;
+import com.liferay.portal.kernel.util.StringPool;
+
+public class VideoEmbedderDisplayContextSimpleFactory {
+
+	public static VideoEmbedderDisplayContext create(HttpServletRequest request, PortletPreferences portletPreferences, VideoEmbedderConfiguration configuration) {
+
+		String url = portletPreferences.getValue("url", StringPool.BLANK);
+
+		if ((url.contains(VideoEmbedderConfiguration.YOUTUBE)) || 
+			(url.contains(VideoEmbedderConfiguration.YOUTUBE_2))) {
+
+			return new YouTubeDisplayContext(request, portletPreferences, configuration);
+		}
+
+		return new BaseVideoEmbedderDisplayContext(request, portletPreferences, configuration);
+  }
+}
