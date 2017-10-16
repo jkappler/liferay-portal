37888f278b172869d91bd9556ffbb3f51ecb6702
arthurchan35 <arthurchan35@gmail.com>
2017-10-16 17:31:06 +0800

    LPS-28940 modify portlet class to handle configuration and rename class

--- modules/apps/youtube/youtube-web/src/main/java/com/liferay/youtube/web/internal/portlet/VideoEmbedderPortlet.java
+++ modules/apps/youtube/youtube-web/src/main/java/com/liferay/youtube/web/internal/portlet/VideoEmbedderPortlet.java
@@ -14,40 +14,69 @@
 
 package com.liferay.youtube.web.internal.portlet;
 
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.youtube.web.internal.constants.YouTubePortletKeys;
+import com.liferay.youtube.web.configuration.VideoEmbedderConfiguration;
+
+import java.io.IOException;
+import java.util.Map;
 
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Peter Fellwock
+ * @author arthurchan35
  */
 @Component(
+	configurationPid = "com.liferay.youtube.web.configuration.VideoEmbedderConfiguration",
 	immediate = true,
 	property = {
-		"com.liferay.portlet.css-class-wrapper=youtube-portlet",
+		"com.liferay.portlet.css-class-wrapper=videoembedder-portlet",
 		"com.liferay.portlet.display-category=category.entertainment",
 		"com.liferay.portlet.icon=/icons/youtube.png",
 		"com.liferay.portlet.use-default-template=true",
-		"javax.portlet.display-name=YouTube",
+		"javax.portlet.display-name=Video Embedder",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + YouTubePortletKeys.YOUTUBE,
-		"javax.portlet.portlet-info.keywords=YouTube",
-		"javax.portlet.portlet-info.short-title=YouTube",
-		"javax.portlet.portlet-info.title=YouTube",
+		"javax.portlet.portlet-info.keywords=Video",
+		"javax.portlet.portlet-info.short-title=VideoEmbedder",
+		"javax.portlet.portlet-info.title=VideoEmbedder",
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
 		"javax.portlet.supports.mime-type=text/html"
 	},
 	service = Portlet.class
 )
-public class YouTubePortlet extends MVCPortlet {
+
+public class VideoEmbedderPortlet extends MVCPortlet {
+
+	public void render(RenderRequest request, RenderResponse response)
+		throws IOException, PortletException {
+		request.setAttribute(VideoEmbedderConfiguration.class
+			.getName(), _configuration);
+
+		super.render(request, response);
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_configuration =
+			(VideoEmbedderConfiguration)ConfigurableUtil.createConfigurable(
+				VideoEmbedderConfiguration.class, properties);
+	}
 
 	@Reference(
 		target = "(&(release.bundle.symbolic.name=com.liferay.youtube.web)(release.schema.version=1.0.0))",
@@ -56,4 +85,5 @@ public class YouTubePortlet extends MVCPortlet {
 	protected void setRelease(Release release) {
 	}
 
+	private volatile VideoEmbedderConfiguration _configuration;
 }
