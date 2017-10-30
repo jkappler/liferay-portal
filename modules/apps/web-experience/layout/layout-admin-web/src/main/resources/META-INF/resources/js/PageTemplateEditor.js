9662574141e89f0549e94861398f566658157f13
Pablo Molina <pablo.molina@liferay.com>
2017-10-30 16:38:54 +0100

    LPS-75540 Added PageTemplateEditor metal component

---
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/resources/META-INF/resources/js/PageTemplateEditor.js
@@ -0,0 +1,96 @@
+import Component from 'metal-component';
+import { Config } from 'metal-state';
+import Soy from 'metal-soy';
+
+import './PageTemplateFragment';
+import './PageTemplateFragmentCollection';
+import templates from './PageTemplateEditor.soy';
+
+/**
+ * Component that allows creating/editing Page Templates
+ */
+class PageTemplateEditor extends Component {
+	/**
+	 * Callback executed when a fragment entry of a collection is clicked.
+	 * It receives fragmentId and fragmentName as event data.
+	 * @param {Event} event
+	 * @private
+	 */
+	_handleFragmentCollectionEntryClick (event) {
+		this.fragments = [
+			...this.fragments,
+			{
+				id: event.fragmentId,
+				name: event.fragmentName,
+				config: {}
+			}
+		];
+	}
+}
+
+/**
+ * State definition.
+ * @type {!Object}
+ * @static
+ */
+PageTemplateEditor.STATE = {
+	/**
+	 * Available entries that can be dragged inside the existing Page Template,
+	 * organized by fragment categories.
+	 * @default undefined
+	 * @instance
+	 * @memberOf PageTemplateEditor
+	 * @type {!Array<object>}
+	 */
+	fragmentCollections: Config.arrayOf(
+		Config.shapeOf({
+			id: Config.string().required(),
+			name: Config.string().required(),
+			entries: Config.arrayOf(
+				Config.shapeOf({
+					id: Config.string().required(),
+					name: Config.string().required()
+				})
+			).required()
+		})
+	).required(),
+
+	/**
+	 * List of fragment instances part of the Page Template, the order of the
+	 * elements in this array defines their position.
+	 * @default []
+	 * @instance
+	 * @memberOf PageTemplateEditor
+	 * @type {Array<string>}
+	 */
+	fragments: Config.arrayOf(
+		Config.shapeOf({
+			id: Config.string().required(),
+			name: Config.string().required(),
+			config: Config.object().value({})
+		})
+	).value([]),
+
+	/**
+	 * Portlet namespace needed for prefixing form inputs
+	 * @default undefined
+	 * @instance
+	 * @memberOf PageTemplateEditor
+	 * @type {!string}
+	 */
+	portletNamespace: Config.string().required(),
+
+	/**
+	 * Path of the available icons.
+	 * @default undefined
+	 * @instance
+	 * @memberOf PageTemplateEditor
+	 * @type {!string}
+	 */
+	spritemap: Config.string().required()
+};
+
+Soy.register(PageTemplateEditor, templates);
+
+export { PageTemplateEditor }
+export default PageTemplateEditor;
