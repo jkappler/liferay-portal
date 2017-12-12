33c3af9f05251381d794e871a865ae570c696a38
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-12-12 12:53:24 +0100

    LPS-76398 Register StagedAssetLinkModelAdapterBuilder as component

--- modules/apps/web-experience/asset/asset-link-service/src/main/java/com/liferay/asset/link/model/adapter/builder/StagedAssetLinkModelAdapterBuilder.java
+++ modules/apps/web-experience/asset/asset-link-service/src/main/java/com/liferay/asset/link/model/adapter/builder/StagedAssetLinkModelAdapterBuilder.java
@@ -19,9 +19,12 @@ import com.liferay.asset.link.model.adapter.StagedAssetLink;
 import com.liferay.asset.link.model.adapter.impl.StagedAssetLinkImpl;
 import com.liferay.portal.kernel.model.adapter.builder.ModelAdapterBuilder;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Mate Thurzo
  */
+@Component(immediate = true, service = ModelAdapterBuilder.class)
 public class StagedAssetLinkModelAdapterBuilder
 	implements ModelAdapterBuilder<AssetLink, StagedAssetLink> {
 
