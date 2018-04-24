373bc4da99aeeea8a57c551b8ceea10dc620ba2a
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-04-24 18:34:34 +0200

    LPS-79703 New selector criterion and handler for display pages

---
+++ modules/apps/web-experience/layout/layout-item-selector-api/src/main/java/com/liferay/layout/item/selector/criterion/DisplayPageSelectorCriterion.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.item.selector.criterion;
+
+import com.liferay.item.selector.BaseItemSelectorCriterion;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class DisplayPageSelectorCriterion extends BaseItemSelectorCriterion {
+
+	public DisplayPageSelectorCriterion() {
+		_classNameId = 0;
+		_classTypeId = 0;
+	}
+
+	public long getClassNameId() {
+		return _classNameId;
+	}
+
+	public long getClassTypeId() {
+		return _classTypeId;
+	}
+
+	public void setClassNameId(long classNameId) {
+		_classNameId = classNameId;
+	}
+
+	public void setClassTypeId(long classTypeId) {
+		_classTypeId = classTypeId;
+	}
+
+	private long _classNameId;
+	private long _classTypeId;
+
+}
