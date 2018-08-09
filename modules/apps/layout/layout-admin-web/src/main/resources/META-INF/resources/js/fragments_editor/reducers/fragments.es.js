ddb931c91c995b6c76925fe56ef46f65f810d932
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-08-09 10:54:58 +0200

    LPS-84225 Adds classNameId and classPK parameters to fragments editor

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/reducers/fragments.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/reducers/fragments.es.js
@@ -85,9 +85,11 @@ function removeFragmentEntryLinkReducer(state, actionType, payload) {
 				_removeFragmentEntryLink(
 					state.deleteFragmentEntryLinkURL,
 					state.portletNamespace,
+					state.classNameId,
+					state.classPK,
 					fragmentEntryLinkId
 				).then(
-					() => {
+					(response) => {
 						const index = state.fragmentEntryLinks.findIndex(
 							fragmentEntryLink => (
 								fragmentEntryLink.fragmentEntryLinkId === fragmentEntryLinkId
@@ -217,9 +219,14 @@ function _getFragmentEntryLinkContent(
 function _removeFragmentEntryLink(
 	deleteFragmentEntryLinkURL,
 	portletNamespace,
+	classNameId,
+	classPK,
 	fragmentEntryLinkId
 ) {
 	const formData = new FormData();
+
+	formData.append(`${portletNamespace}classNameId`, classNameId);
+	formData.append(`${portletNamespace}classPK`, classPK);
 
 	formData.append(
 		`${portletNamespace}fragmentEntryLinkId`,
