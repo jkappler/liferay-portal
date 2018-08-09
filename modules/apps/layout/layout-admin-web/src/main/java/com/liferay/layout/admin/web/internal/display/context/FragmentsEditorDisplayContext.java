d3961705787a26bdf678e68c96d6fae89cf2d615
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-08-09 09:59:07 +0200

    LPS-84225 Add settings to variables available to the editor

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/FragmentsEditorDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/FragmentsEditorDisplayContext.java
@@ -37,7 +37,9 @@ import com.liferay.item.selector.criteria.url.criterion.URLItemSelectorCriterion
 import com.liferay.layout.admin.web.internal.constants.LayoutAdminWebKeys;
 import com.liferay.layout.admin.web.internal.util.SoyContextFactoryUtil;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.model.LayoutPageTemplateSetting;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryServiceUtil;
+import com.liferay.layout.page.template.service.LayoutPageTemplateSettingLocalServiceUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.editor.configuration.EditorConfiguration;
 import com.liferay.portal.kernel.editor.configuration.EditorConfigurationFactoryUtil;
@@ -156,6 +158,10 @@ public class FragmentsEditorDisplayContext {
 		soyContext.put("imageSelectorURL", itemSelectorURL.toString());
 
 		soyContext.put("languageId", _themeDisplay.getLanguageId());
+
+		soyContext.put(
+			"layoutSettings",
+			JSONFactoryUtil.createJSONObject(_getLayoutSettings()));
 
 		if (_showMapping) {
 			soyContext.put(
@@ -310,6 +316,19 @@ public class FragmentsEditorDisplayContext {
 				_classPK);
 
 		return _layoutPageTemplateEntry;
+	}
+
+	private String _getLayoutSettings() {
+		LayoutPageTemplateSetting layoutPageTemplateSetting =
+			LayoutPageTemplateSettingLocalServiceUtil.
+				fetchLayoutPageTemplateSetting(
+					_themeDisplay.getScopeGroupId(), _classNameId, _classPK);
+
+		if (layoutPageTemplateSetting != null) {
+			return layoutPageTemplateSetting.getSettings();
+		}
+
+		return StringPool.BLANK;
 	}
 
 	private String _getMappingSubtypeLabel() throws PortalException {
