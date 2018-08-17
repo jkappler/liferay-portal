51877303c3c09c98ba92012959ee0f0cc8b94c5f
Pablo Molina <contact@pablomolina.me>
2018-08-17 11:54:57 +0200

    LPS-84068 Allow removing fragments using store

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/store/actionTypes.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/store/actionTypes.es.js
@@ -15,6 +15,14 @@ const ADD_FRAGMENT_ENTRY_LINK = 'addFragmentEntryLink';
 const CLEAR_DRAG_TARGET = 'clearDragTarget';
 
 /**
+ * Remove fragment entry link action type
+ * @review
+ * @type {string}
+ */
+
+const REMOVE_FRAGMENT_ENTRY_LINK = 'removeFragmentEntryLink';
+
+/**
  * Update drag target action type
  * @review
  * @type {string}
@@ -25,5 +33,6 @@ const UPDATE_DRAG_TARGET = 'updateDragTarget';
 export {
 	ADD_FRAGMENT_ENTRY_LINK,
 	CLEAR_DRAG_TARGET,
+	REMOVE_FRAGMENT_ENTRY_LINK,
 	UPDATE_DRAG_TARGET
 };
