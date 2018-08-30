0b9789a817aece15ab08dd9d3be2dbd527044cc5
Pablo Molina <contact@pablomolina.me>
2018-08-30 12:49:15 +0200

    LPS-84225 Use structure attribute for Layout fragment positions

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/store/reducers/translationStatus.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/store/reducers/translationStatus.js
@@ -31,7 +31,10 @@ function translationStatusReducer(state, actionType) {
  */
 
 function _getEditableValues(fragmentEntryLinks) {
-	return fragmentEntryLinks
+	return Object.values(fragmentEntryLinks)
+		.filter(
+			fragmentEntryLink => fragmentEntryLink.editableValues
+		)
 		.map(
 			fragmentEntryLink => (
 				fragmentEntryLink.editableValues[EDITABLE_VALUES_KEY]
