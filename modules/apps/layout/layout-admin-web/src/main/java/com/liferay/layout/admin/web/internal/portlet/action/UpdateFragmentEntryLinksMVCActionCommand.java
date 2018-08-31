53029a7c09a3b01fc9eff5722cb519f05faf2840
Pablo Molina <contact@pablomolina.me>
2018-08-31 10:08:34 +0200

    LPS-84225 Update UpdateFragmentEntryLinksMVCActionCommand

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/UpdateFragmentEntryLinksMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/UpdateFragmentEntryLinksMVCActionCommand.java
@@ -93,12 +93,6 @@ public class UpdateFragmentEntryLinksMVCActionCommand
 	protected void updateFragmentEntryLinks(ActionRequest actionRequest)
 		throws PortalException {
 
-		long fragmentEntryLinkId1 = ParamUtil.getLong(
-			actionRequest, "fragmentEntryLinkId1");
-
-		long fragmentEntryLinkId2 = ParamUtil.getLong(
-			actionRequest, "fragmentEntryLinkId2");
-
 		long classNameId = ParamUtil.getLong(actionRequest, "classNameId");
 		long classPK = ParamUtil.getLong(actionRequest, "classPK");
 		String settings = ParamUtil.getString(actionRequest, "settings");
@@ -106,26 +100,10 @@ public class UpdateFragmentEntryLinksMVCActionCommand
 		ServiceContext serviceContext = ServiceContextFactory.getInstance(
 			actionRequest);
 
-		FragmentEntryLink fragmentEntryLink1 =
-			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
-				fragmentEntryLinkId1);
-
-		FragmentEntryLink fragmentEntryLink2 =
-			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
-				fragmentEntryLinkId2);
-
-		if ((fragmentEntryLink1 != null) && (fragmentEntryLink2 != null)) {
-			_fragmentEntryLinkLocalService.updateFragmentEntryLink(
-				fragmentEntryLinkId1, fragmentEntryLink2.getPosition());
-
-			_fragmentEntryLinkLocalService.updateFragmentEntryLink(
-				fragmentEntryLinkId2, fragmentEntryLink1.getPosition());
-
-			_layoutPageTemplateSettingLocalService.
-				updateLayoutPageTemplateSetting(
-					serviceContext.getScopeGroupId(), classNameId, classPK,
-					settings);
-		}
+		_layoutPageTemplateSettingLocalService.
+			updateLayoutPageTemplateSetting(
+				serviceContext.getScopeGroupId(), classNameId, classPK,
+				settings);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
