6d0da00dce3bd0da87c55805c586e13037230382
Pablo Molina <pablo.molina@liferay.com>
2018-09-03 10:52:19 +0200

    LPS-84225 Move updateFragmentEntryLinksURL to store

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/FragmentsEditor.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/FragmentsEditor.es.js
@@ -644,17 +644,6 @@ FragmentsEditor.STATE = Object.assign(
 		spritemap: Config.string().required(),
 
 		/**
-		 * URL for swapping to fragmentEntryLinks.
-		 * @default undefined
-		 * @instance
-		 * @memberOf FragmentsEditor
-		 * @review
-		 * @type {!string}
-		 */
-
-		updateFragmentEntryLinksURL: Config.string().required(),
-
-		/**
 		 * URL for updating the asset type associated to a template.
 		 * @default undefined
 		 * @instance
