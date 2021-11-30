0325503019a921fc7d71abfbea5a4fab05cc18e5
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-11-30 11:05:38 +0100

    LPS-XXXXX Invalidate cache when removing an asset

--- modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/model/listener/AssetEntryModelListener.java
+++ modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/model/listener/AssetEntryModelListener.java
@@ -15,13 +15,24 @@
 package com.liferay.asset.internal.model.listener;
 
 import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.layout.model.LayoutClassedModelUsage;
 import com.liferay.layout.service.LayoutClassedModelUsageLocalService;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * @author Bryan Engler
@@ -38,6 +49,34 @@ public class AssetEntryModelListener extends BaseModelListener<AssetEntry> {
 	public void onBeforeRemove(AssetEntry assetEntry)
 		throws ModelListenerException {
 
+		PortalCache<String, String> portalCache =
+			(PortalCache<String, String>)_multiVMPool.getPortalCache(
+				FragmentEntryLink.class.getName());
+
+		List<LayoutClassedModelUsage> layoutClassedModelUsages =
+			_layoutClassedModelUsageLocalService.getLayoutClassedModelUsages(
+				assetEntry.getClassNameId(),
+				assetEntry.getClassPK());
+
+		for (LayoutClassedModelUsage layoutClassedModelUsage :
+			layoutClassedModelUsages) {
+
+			for (Locale locale :
+				LanguageUtil.getAvailableLocales(
+					assetEntry.getGroupId())) {
+
+				StringBundler sb = new StringBundler(5);
+
+				sb.append(layoutClassedModelUsage.getContainerKey());
+				sb.append(StringPool.DASH);
+				sb.append(locale);
+				sb.append(StringPool.DASH);
+				sb.append(0);
+
+				portalCache.remove(sb.toString());
+			}
+		}
+
 		_layoutClassedModelUsageLocalService.deleteLayoutClassedModelUsages(
 			assetEntry.getClassNameId(), assetEntry.getClassPK());
 	}
@@ -46,4 +85,10 @@ public class AssetEntryModelListener extends BaseModelListener<AssetEntry> {
 	private LayoutClassedModelUsageLocalService
 		_layoutClassedModelUsageLocalService;
 
+	@Reference
+	private Language _language;
+
+
+	@Reference
+	private MultiVMPool _multiVMPool;
 }
