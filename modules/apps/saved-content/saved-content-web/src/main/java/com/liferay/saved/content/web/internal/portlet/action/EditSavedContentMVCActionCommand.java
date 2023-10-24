4d43b2c38590a95b646f553991aa15f76503eedf
Alicia García <alicia.garcia@liferay.com>
2023-10-24 13:28:20 +0200

    LPS-197916 add the action to mark the element as saved content

--- modules/apps/saved-content/saved-content-web/src/main/java/com/liferay/saved/content/web/internal/portlet/action/EditSavedContentMVCActionCommand.java
+++ modules/apps/saved-content/saved-content-web/src/main/java/com/liferay/saved/content/web/internal/portlet/action/EditSavedContentMVCActionCommand.java
@@ -5,13 +5,22 @@
 
 package com.liferay.saved.content.web.internal.portlet.action;
 
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.saved.content.constants.MySavedContentPortletKeys;
+import com.liferay.saved.content.model.SavedContentEntry;
+import com.liferay.saved.content.service.SavedContentEntryService;
+
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alicia García
@@ -30,8 +39,43 @@ public class EditSavedContentMVCActionCommand extends BaseMVCActionCommand {
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
-
+		JSONPortletResponseUtil.writeJSON(
+			actionRequest, actionResponse,
+			_updateSavedContentEntry(actionRequest));
 	}
 
+	private String _updateSavedContentEntry(ActionRequest actionRequest)
+		throws Exception {
+
+		String className = ParamUtil.getString(actionRequest, "className");
+		long classPK = ParamUtil.getLong(actionRequest, "classPK");
+
+		if (Validator.isNull(className) || (classPK == 0)) {
+			return null;
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long userId = themeDisplay.getUserId();
+
+		SavedContentEntry savedContentEntry =
+			_savedContentEntryService.fetchSavedContentEntry(
+				themeDisplay.getScopeGroupId(), userId, className, classPK);
+
+		if (savedContentEntry == null) {
+			_savedContentEntryService.addSavedContentEntry(
+				themeDisplay.getScopeGroupId(), userId, className, classPK);
+
+			return Boolean.TRUE.toString();
+		}
+
+		_savedContentEntryService.deleteSavedContentEntry(savedContentEntry);
+
+		return Boolean.FALSE.toString();
+	}
+
+	@Reference
+	private SavedContentEntryService _savedContentEntryService;
 
 }
