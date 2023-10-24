1249374ed098064f6b25c7bb3394443521e7afd4
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-10-24 11:32:51 +0200

    LPS-199840 Adds new method to specify the windows state for configuration actions and make the default a pop up to have backwards compatibility. This will give us more flexibility on how we want configurations to be shown to users, and not only have modals

--- portal-kernel/src/com/liferay/portal/kernel/portlet/ConfigurationAction.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/ConfigurationAction.java
@@ -8,6 +8,7 @@ package com.liferay.portal.kernel.portlet;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
+import javax.portlet.WindowState;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,10 @@ import javax.servlet.http.HttpServletResponse;
  * @author Brian Wing Shun Chan
  */
 public interface ConfigurationAction {
+
+	public default WindowState getWindowsState() {
+		return LiferayWindowState.POP_UP;
+	}
 
 	public void include(
 			PortletConfig portletConfig, HttpServletRequest httpServletRequest,
