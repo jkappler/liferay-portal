e54c57cf6520b0f3869f13f0c495c5a3dabcffc5
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-10-24 11:39:26 +0200

    LPS-199840 New configuration icon that will redirect the user to a new configuration view instead of a modal

---
+++ modules/apps/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/portlet/configuration/icon/BrowseConfigurationPortletConfigurationIcon.java
@@ -0,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portlet.configuration.web.internal.portlet.configuration.icon;
+
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HttpComponentsUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.WindowState;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = PortletConfigurationIcon.class)
+public class BrowseConfigurationPortletConfigurationIcon
+	extends BaseConfigurationPortletConfigurationIcon {
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		WindowState windowState = portletDisplay.getWindowStateConfiguration();
+
+		return HttpComponentsUtil.addParameter(
+			portletDisplay.getUrlConfigurationBrowse(), "p_p_state",
+			windowState.toString());
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (layout.isEmbeddedPersonalApplication()) {
+			return false;
+		}
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		if (portletDisplay.getWindowStateConfiguration() ==
+				LiferayWindowState.POP_UP) {
+
+			return false;
+		}
+
+		return portletDisplay.isShowConfigurationIcon();
+	}
+
+}
