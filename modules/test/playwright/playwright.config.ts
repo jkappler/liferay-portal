b7f845a0d1e61ec0d23e2e33210feb3f7eca688f
Sandro Chinea <sandrochinea@gmail.com>
2024-05-24 11:53:18 +0100

    LPD-25954 Add ContentDashboardPage

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -15,6 +15,7 @@ import {config as blogsWebConfig} from './tests/blogs-web/config';
 import {config as changeTrackingWebConfig} from './tests/change-tracking-web/config';
 import {config as clientExtensionWebConfig} from './tests/client-extension-web/config';
 import {config as commerceConfig} from './tests/commerce/config';
+import {config as contentDashboardConfig} from './tests/content-dashboard-web/config';
 import {config as cookiesBannerWebConfig} from './tests/cookies-banner-web/config';
 import {config as dispatchWebConfig} from './tests/dispatch-web/config';
 import {config as documentLibraryWebConfig} from './tests/document-library-web/config';
@@ -66,6 +67,7 @@ export default defineConfig({
 		changeTrackingWebConfig,
 		clientExtensionWebConfig,
 		commerceConfig,
+		contentDashboardConfig,
 		dispatchWebConfig,
 		documentLibraryWebConfig,
 		dynamicDataMappingFormWebConfig,
