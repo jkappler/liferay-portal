83185da4b952e9c2860e4d69fec2f6d93a07efc3
Sandro Chinea <sandrochinea@gmail.com>
2024-05-24 11:53:18 +0100

    LPD-25954 Add ContentDashboardPage

--- modules/test/playwright/utils/portletUrls.ts
+++ modules/test/playwright/utils/portletUrls.ts
@@ -6,6 +6,8 @@ export const PORTLET_URLS = {
 	blogs: '/~/control_panel/manage?p_p_id=com_liferay_blogs_web_portlet_BlogsAdminPortlet',
 	collections:
 		'/~/control_panel/manage?p_p_id=com_liferay_asset_list_web_portlet_AssetListPortlet',
+	contentDashboard:
+		'/~/control_panel/manage?p_p_id=com_liferay_content_dashboard_web_portlet_ContentDashboardAdminPortlet',
 	displayPageTemplates:
 		'/~/control_panel/manage?p_p_id=com_liferay_layout_page_template_admin_web_portlet_LayoutPageTemplatesPortlet&_com_liferay_layout_page_template_admin_web_portlet_LayoutPageTemplatesPortlet_tabs1=display-page-templates',
 	documentLibrary:
