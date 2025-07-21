618c9dfcc25a1df9b0c7ebf82b67e6c9e2eb0b6b
Mikel Lorza <mikel.lorza@liferay.com>
2025-07-21 12:20:12 +0200

    LPD-58489 Take into account when we are returning an object entry version wrapper into an object entry in order to add the object entry version actions

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -751,13 +751,18 @@ public class DefaultObjectEntryManagerImpl
 		com.liferay.object.model.ObjectEntry serviceBuilderObjectEntry =
 			_objectEntryService.getObjectEntry(objectEntryId);
 
-		dtoConverterContext.setAttribute(
-			"objectEntryVersion",
+		ObjectEntryVersion objectEntryVersion =
 			_objectEntryVersionService.getObjectEntryVersion(
-				objectEntryId, version));
+				objectEntryId, version);
+
+		dtoConverterContext.setAttribute(
+			"objectEntryVersion", objectEntryVersion);
 
 		return _objectEntryDTOConverter.toDTO(
-			dtoConverterContext, serviceBuilderObjectEntry);
+			_getObjectEntryVersionDTOConverterContext(
+				dtoConverterContext, objectEntryVersion,
+				serviceBuilderObjectEntry),
+			serviceBuilderObjectEntry);
 	}
 
 	@Override
@@ -1000,7 +1005,8 @@ public class DefaultObjectEntryManagerImpl
 		return _restoreVersionedObjectEntry(
 			dtoConverterContext, objectDefinition,
 			getObjectEntryByVersion(
-				dtoConverterContext, objectEntryId, version));
+				dtoConverterContext, objectEntryId, version),
+			version);
 	}
 
 	@Override
@@ -1014,7 +1020,8 @@ public class DefaultObjectEntryManagerImpl
 			dtoConverterContext, objectDefinition,
 			getObjectEntryByVersion(
 				dtoConverterContext, externalReferenceCode, objectDefinition,
-				scopeKey, version));
+				scopeKey, version),
+			version);
 	}
 
 	@Override
@@ -1509,13 +1516,18 @@ public class DefaultObjectEntryManagerImpl
 				serviceBuilderObjectEntry.getObjectEntryId()),
 			dtoConverterContext.getUserId(), version);
 
-		dtoConverterContext.setAttribute(
-			"objectEntryVersion",
+		ObjectEntryVersion objectEntryVersion =
 			_objectEntryVersionService.getObjectEntryVersion(
-				serviceBuilderObjectEntry.getObjectEntryId(), version));
+				serviceBuilderObjectEntry.getObjectEntryId(), version);
+
+		dtoConverterContext.setAttribute(
+			"objectEntryVersion", objectEntryVersion);
 
 		return _objectEntryDTOConverter.toDTO(
-			dtoConverterContext, serviceBuilderObjectEntry);
+			_getObjectEntryVersionDTOConverterContext(
+				dtoConverterContext, objectEntryVersion,
+				serviceBuilderObjectEntry),
+			serviceBuilderObjectEntry);
 	}
 
 	private String _getDateString(Date date) {
@@ -2170,14 +2182,19 @@ public class DefaultObjectEntryManagerImpl
 
 	private ObjectEntry _restoreVersionedObjectEntry(
 			DTOConverterContext dtoConverterContext,
-			ObjectDefinition objectDefinition, ObjectEntry objectEntry)
+			ObjectDefinition objectDefinition, ObjectEntry objectEntry,
+			int version)
 		throws Exception {
 
 		_removeReadOnlyProperties(objectDefinition, objectEntry);
 
 		return updateObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntry.getId(),
-			objectEntry);
+			_getObjectEntryVersionDTOConverterContext(
+				dtoConverterContext,
+				_objectEntryVersionService.getObjectEntryVersion(
+					objectEntry.getId(), version),
+				_objectEntryService.getObjectEntry(objectEntry.getId())),
+			objectDefinition, objectEntry.getId(), objectEntry);
 	}
 
 	private Date _toDate(Locale locale, String valueString) {
